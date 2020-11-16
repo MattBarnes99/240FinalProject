6bf5de395f03f38cabd184e92be3892f71d925af
Katie <katie.wall@richmond.edu>
2020-11-15 21:59:53 -0500

    wrote TrafficLight.cpp and TrafficLight.h

--- TrafficLight.cpp
+++ TrafficLight.cpp
@@ -1,1 +1,22 @@
+#ifndef __TRAFFICLIGHT_CPP__
+#define __TRAFFICLIGHT_CPP__
 
+#include "TrafficLight.h"
+
+using namespace std;
+
+TrafficLight::TrafficLight(String light_color, int green, int yellow)
+	: color{light_color},
+	  greenLength{green},
+	  yellowLength{yellow}
+	{}
+
+TrafficLight::TrafficLight(const TrafficLight& other)
+	: color{other.color},
+	  greenLength{other.greenLength},
+	  yellowLength{other.yellowLength}
+	{}
+
+TrafficLight::~TrafficLight() {}
+
+#endif
