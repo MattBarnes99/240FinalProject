fa7d7ba179042d9688d97d8d7c09c8963e109523
md5zr <md5zr@mathcs01.richmond.edu>
2020-11-16 11:54:30 -0500

    Added to Clock.h and Clock.cpp

--- Clock.cpp
+++ Clock.cpp
@@ -1,1 +1,42 @@
+#ifndef __CLOCK_CPP__
+#define __CLOCK_CPP__
+
+#include "Clock.h"
+
+using namespace std;
+
+Clock::Clock(int time):counter{time}{}
+
+Clock::~Clock(){}
+
+void Clock::increment(){
+	if(cin.get() == '\n'){
+		counter++;
+	}
+}
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Clock.h
+++ Clock.h
@@ -1,1 +1,41 @@
+#ifndef __CLOCK_H__
+#define __CLOCK_H__
+
+using namespace std;
+
+class Clock{
+	private:
+		int counter;
+	public:
+		Clock(int time);
+		~Clock();
+
+		void increment();
+		
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
