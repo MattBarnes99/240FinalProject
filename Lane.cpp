07a11b5807109d1a3977c511dc401350560468da
Katie <katie.wall@richmond.edu>
2020-11-15 22:18:23 -0500

    wrote Lane.h and Lane.cpp

--- Lane.cpp
+++ Lane.cpp
@@ -1,1 +1,18 @@
+#ifndef __LANE_CPP__
+#define __LANE_CPP__
 
+#include "Lane.h"
+
+using namespace std;
+
+Lane::Lane(int sections) {
+	lane.resize(sections);
+}
+
+Lane::Lane(const Lane& other) {
+	lane = other.lane;
+}
+
+Lane::~Lane() {}
+
+#endif
--- Lane.h
+++ Lane.h
@@ -1,1 +1,18 @@
+#ifndef __LANE_H__
+#define __LANE_H__
 
+#include "Vehicle.h"
+#include <vector>
+
+using namespace std;
+
+class Lane {
+	public:
+		vector<Vehicle> lane;
+
+		Lane(int sections);
+		Lane(const Lane& other);
+		~Lane();
+}
+
+#endif
