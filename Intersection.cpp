5d45f61b99042a457c000e918eeaffb6cb0306a7
Katie <katie.wall@richmond.edu>
2020-11-18 20:44:24 -0500

    made Intersection.cpp and Intersection.h

---
+++ Intersection.cpp
@@ -0,0 +1,10 @@
+#ifndef __INTERSECTION_CPP__
+#define __INTERSECTION_CPP__
+
+#include "Intersection.h"
+
+using namespace std;
+
+Intersection::Intersection() : Section() {};
+
+Intersection::~Intersection() {};
